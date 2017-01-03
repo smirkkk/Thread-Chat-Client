736caa44a75788718d809ba9cadea42f157603a3
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-01-04 08:58:47 +0900

    Module

---
+++ Module/mysocket.py
@@ -0,0 +1,30 @@
+import socket
+
+def sendMsg(socket,string, key):
+    string = cryption(string, key)
+    socket.sendall(str.encode(string+chr(0)))
+
+def getMsg(socket, key):
+    string=''
+
+    while True:
+        data = socket.recv(1)
+        data=data.decode("utf-8")
+
+        if data == chr(0):
+            break
+        string+=data
+
+    string = cryption(string, key)
+    return string
+
+def cryption(string, key):
+    trash = ''
+    tmp = ''
+
+    for x in range(len(string)):
+        tmp = ord(string[x])
+        tmp ^= key
+        trash += chr(tmp)
+
+    return trash
