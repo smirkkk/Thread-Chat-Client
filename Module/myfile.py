395466ad614030f32cfaf705cb0a8dd5c984bb4a
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-01-06 16:14:13 +0900

    Add extention method

--- Module/myfile.py
+++ Module/myfile.py
@@ -1,5 +1,18 @@
 import boto3
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QFileDialog
+
+def make_extention(file):
+    blank = ''
+    extention = file.split('/')
+    extention = extention[-1]
+
+    if extention.find('.') == -1:
+        return blank
+    else:
+        extention = extention.split('.')
+        extention = extention[-1]
+        extention = '.'+extention
+        return extention
 
 def openFileNameDialog(self):
     options = QFileDialog.Options()
@@ -10,10 +23,14 @@ def openFileNameDialog(self):
 
 def uploadFile(self, file_path, file_name, folder=None, bucket='dsm-chatting'):
     s3 = boto3.resource('s3')
+    print(file_path)
+    extender = make_extention(file_path)
+    print(extender)
     s3_path = None
     if folder:
-        s3_path = folder + file_name
-    else :
-        s3_path = file_name
+        s3_path = folder + '/' + file_name + extender
+    else:
+        s3_path = file_name + extender
 
+    print(s3_path)
     s3.meta.client.upload_file(file_path, bucket, s3_path)
