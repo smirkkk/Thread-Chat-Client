5eefe8bfa43b78a647edc9165027b599fffbe1bf
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-01-06 16:10:36 +0900

    File upload module

--- Module/myfile.py
+++ Module/myfile.py
@@ -6,10 +6,14 @@ def openFileNameDialog(self):
     options |= QFileDialog.DontUseNativeDialog
     fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
                                               "All Files (*);;Python Files (*.py)", options=options)
-
     return fileName
 
-
-def uploadFile(self, filename):
+def uploadFile(self, file_path, file_name, folder=None, bucket='dsm-chatting'):
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(filename, 'dsm-chatting', 'qwerty')
+    s3_path = None
+    if folder:
+        s3_path = folder + file_name
+    else :
+        s3_path = file_name
+
+    s3.meta.client.upload_file(file_path, bucket, s3_path)
