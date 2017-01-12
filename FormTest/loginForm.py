c9698d48b130c78f35e8dbd24d3948e1b515b967
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-01-12 22:53:41 +0900

    no message

--- FormTest/loginForm.py
+++ FormTest/loginForm.py
@@ -1,15 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLineEdit, QLabel, QVBoxLayout, QTabWidget, QMainWindow, QScrollArea
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLineEdit, QLabel, QVBoxLayout, QTabWidget, QMainWindow, QCheckBox, QScrollArea, QTextEdit, QGroupBox
+from PyQt5.QtCore import Qt
 import socket
 import myfile
 import mysocket
 import mysign
 import threading
+import datetime
+import traceback
 
 HOST = "192.168.137.191"
-PORT = 6974
+PORT = 33333
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+
 key = 23
 
 user_info = {
@@ -17,13 +22,17 @@ user_info = {
     'account': ''
 }
 
-friend_list = []
+tmp = [] #채팅방 만들 때 닉네임 저장
+tmp2 = [] #채팅방 만들 때 아이디 저장
+name_tmp = [] #친구목록 불러올때 닉네임
+btn_tmp = [] #친구목록 불러올때 버튼
+friend_list = [] #친구 목록
+chat_list = [] #채팅방 목록
 
-## close add need
+
 
 class Login(QWidget):
     def __init__(self):
-        
         super().__init__()
 
     def loginUi(self):
@@ -219,63 +228,165 @@ class SignUp(QWidget):
 
 
 class MainForm(QMainWindow):
+
     def draw(self):
-        self.setGeometry(300, 100, 300, 500)
+        self.setGeometry(300, 100, 400, 550)
+        self.setWindowTitle(user_info['username'])
         self.table_widget = MainWidget(self)
         self.setCentralWidget(self.table_widget)
+
         self.show()
 
 
 class Chatting(QWidget):
+    chat_socket = None
+    index = 0
+
+
     def draw(self):
-        string = '15 나성범 RF'
-        self.setGeometry(124, 200, 300, 500)
-        self.setWindowTitle(string)
+        try:
+            HOST = "192.168.137.191"
+            PORT = 9999
+            self.chat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.chat_socket.connect((HOST, PORT))
 
-        self.chat = QLineEdit(self)
-        self.chat.setReadOnly(True)
-        self.chat.move(5, 5)
-        self.chat.resize(290, 390)
+            threading._start_new_thread(self.a, ())
 
-        self.text = QLineEdit(self)
-        self.text.move(5, 400)
-        self.text.resize(233, 70)
+            mysocket.sendMsg(self.chat_socket, user_info['account'], key)
 
-        btn = QPushButton('전송', self)
+            print(user_info['account'])
 
-        btn.resize(btn.sizeHint())
-        btn.resize(52, 70)
-        btn.move(243, 400)
+            mysocket.sendMsg(self.chat_socket, '13', key)
 
-        btn.clicked.connect(self.send_click)
 
-        attach_btn = QPushButton('파일 첨부', self)
+            self.setGeometry(124, 200, 300, 550)
+            self.setWindowTitle(user_info['username'])
 
-        attach_btn.resize(attach_btn.sizeHint())
-        attach_btn.resize(60, 20)
-        attach_btn.move(5, 475)
+            self.chat_out = QPushButton('나가기', self)
 
-        attach_btn.clicked.connect(self.attach_btn_click)
+            self.chat_out.resize(self.chat_out.sizeHint())
+            self.chat_out.resize(60, 20)
+            self.chat_out.move(235, 5)
 
-        self.show()
+            self.member = QLabel('멤버', self)
+            self.member.move(5, 5)
+
+            self.chat = QTextEdit(self)
+            self.chat.setReadOnly(True)
+            self.chat.move(5, 30)
+            self.chat.resize(290, 410)
+
+            self.text = QTextEdit(self)
+            self.text.move(5, 450)
+            self.text.resize(233, 70)
+            btn = QPushButton('전송', self)
+
+            btn.resize(btn.sizeHint())
+            btn.resize(52, 70)
+            btn.move(243, 450)
+
+            btn.clicked.connect(self.send_click)
+
+            attach_btn = QPushButton('파일 첨부', self)
+
+            attach_btn.resize(attach_btn.sizeHint())
+            attach_btn.resize(60, 20)
+            attach_btn.move(5, 525)
+
+            attach_btn.clicked.connect(self.attach_btn_click)
+
+            down_btn = QPushButton('다운로드', self)
+
+            down_btn.resize(down_btn.sizeHint())
+            down_btn.resize(60, 20)
+            down_btn.move(235, 525)
+
+            down_btn.clicked.connect(self.down_btn_click)
+
+            self.down_link = QLineEdit(self)
+            self.down_link.resize(165, 20)
+            self.down_link.move(70, 525)
+
+            self.show()
+        except Exception as e:
+            print(traceback.format_exc())
+
+    def a(self):
+        line = ''
+        fr = ''
+        dt = ''
+        msg = ''
+        string = ''
+        while True:
+            string = mysocket.getMsg(self.chat_socket, 23)
+            if (self.index+1) % 3 == 0:
+                msg = string
+                line = '{}/{}/{}\n'.format(fr, dt, msg)
+                print(line)
+            elif (self.index+1) % 3 == 1:
+                fr = string
+            elif (self.index+1) % 3 == 2:
+                dt = string
+
+                #self.chat.setPlainText(line)
+
+            self.index += 1
+
+        # chat_string = self.chat.toPlainText() + user_info['username'] + " : " + self.text.toPlainText() + '\n'
+
 
     def send_click(self):
-        mysocket.sendMsg(s, self.text.text(), key)
-        a = self.chat.text() + '\n' + "me : "+self.text.text()
-        self.chat.setText(a)
-        self.text.setText("")
+        if self.text.toPlainText() == '':
+            return
+        else:
+            """
+            now = datetime.datetime.now()
+            now = '[' + str(now.hour) + ':' + str(now.minute) + ']'
+            chat_string = self.chat.toPlainText() + '\n' + now + ' '+user_info['username'] +" : "+ self.text.toPlainText()
+            """
+
+            chat_string = self.chat.toPlainText() + user_info['username'] + " : " + self.text.toPlainText() +'\n'
+
+            mysocket.sendMsg(self.chat_socket, self.text.toPlainText(), key)
+
+            self.chat.setPlainText(chat_string)
+            self.text.setPlainText("")
+
+    def getget(self):
+        return threading._start_new_thread(mysocket.getMsg, (self.chat_socket, key))
+
+
+
 
     def attach_btn_click(self):
         nametest = 'test'
+
         file = myfile.openFileNameDialog(self)
+
+        to_append = myfile.make_extention(file)
+        to_append = nametest + to_append
+
         myfile.uploadFile(self, file_path=file, file_name=nametest, folder='')
-        self.chat.setText('일단 업로드 성공~!@#45ㅁ6')
+
+        a = self.chat.toPlainText() + '\n\n' + user_info['account']+"님이 파일을 전송하셨습니다 : "+to_append + '\n\n'
+
+        self.chat.setPlainText(a)
+
+    def down_btn_click(self):
+        file_link = self.down_link.text()
+        self.down_link.setText("")
+        myfile.downloadFile(self,'chattingroomname',file_link)
 
 
 class MainWidget(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
+
+        friend_list = self.refresh_click()
+        chat_list = self.room_refresh_btn_click()
+
+        self.parent = parent
 
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
@@ -294,12 +405,29 @@ class MainWidget(QWidget):
         self.tab1.setLayout(self.tab1.layout)
         self.refresh_btn.clicked.connect(self.refresh_click)
 
+        try:
+            for x in range(int(len(friend_list)/2)):
+                self.tab1.layout.addWidget(QLabel(friend_list[x * 2 + 1]))
+        except Exception as e:
+            print(e)
+
         self.tab2.layout = QVBoxLayout(self)
-        self.pushButton3 = QPushButton("1")
+
+        self.room_refresh_btn = QPushButton("채팅목록 새로고침")
+        self.make_btn = QPushButton('채팅방 만들기')
+
         self.pushButton4 = QPushButton("2")
-        self.tab2.layout.addWidget(self.pushButton3)
+
+        self.tab2.layout.addWidget(self.room_refresh_btn)
+        self.tab2.layout.addWidget(self.make_btn)
         self.tab2.layout.addWidget(self.pushButton4)
+        self.tab2.layout.addWidget(self.room_box(chat_list))
+
         self.tab2.setLayout(self.tab2.layout)
+
+        self.room_refresh_btn.clicked.connect(self.room_refresh_btn_click)
+        self.make_btn.clicked.connect(self.make_click)
+        self.pushButton4.clicked.connect(self.btn_click)
 
         self.tab3.layout = QVBoxLayout(self)
 
@@ -313,16 +441,12 @@ class MainWidget(QWidget):
         self.tab3.layout.addWidget(self.myID)
         self.tab3.layout.addWidget(self.to_search)
 
-
         self.tab3.setLayout(self.tab3.layout)
 
         self.to_search.clicked.connect(self.move_to_search)
 
-
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
-
-        self.pushButton4.clicked.connect(self.btn_click)
 
         self.tab4.layout = QVBoxLayout(self)
         self.withdraw_btn = QPushButton('탈퇴')
@@ -345,14 +469,25 @@ class MainWidget(QWidget):
 
         while True:
             string = mysocket.getMsg(s, key)
-            if string == 'end' or string == 'end ':
+            if string == 'end!' or string == 'end! ':
                 break
             else:
                 friend_list.append(string)
 
+        try:
+            for i, children in enumerate(self.tab1.findChildren(QLabel)):
+                #print(type(children), children, children.objectName())
+                children.deleteLater()
+            for x in range(int(len(friend_list)/2)):
+                self.tab1.layout.addWidget(QLabel(friend_list[x * 2 + 1]))
+        except Exception as e:
+            pass
+
         print(user_info['username'])
         print(user_info['account'])
         print(friend_list)
+
+        return  friend_list
 
     def move_to_search(self):
         self.tmp = searchID()
@@ -368,9 +503,65 @@ class MainWidget(QWidget):
             return
         else:
             QMessageBox.information(self, '로그아웃', '로그아웃')
-            #mysocket.sendMsg(s, '8', key)
-            #로그인 정보 비우고
-            #로그인 폼으로 이동.
+            mysocket.sendMsg(s, '8', key)
+            self.move_to_login()
+            user_info['username'] = ''
+            user_info['account'] = ''
+
+    def move_to_login(self):
+        self.parent.hide()
+        ex.run()
+
+    def make_click(self):
+        self.tmp = makeRoom()
+        self.tmp.makeroomUi()
+
+    def room_refresh_btn_click(self):
+
+        chat_list = []
+
+        mysocket.sendMsg(s, '9', key)
+
+        while True:
+            string = mysocket.getMsg(s, key)
+            if string == 'end!' or string == 'end! ':
+                break
+            else:
+                chat_list.append(string)
+
+        print(chat_list)
+        self.room_box(chat_list)
+
+        return chat_list
+
+    def room_box(self, chat_list):
+
+        groupBox = QGroupBox("채팅방 목록")
+
+        vbox = QVBoxLayout()
+        """
+        try:
+            for i, children in enumerate(vbox.findChildren(QPushButton)):
+                children.deleteLater()
+            for x in range(int(len(chat_list) / 2)):
+                vbox.addWidget(QPushButton(chat_list[x * 2 + 1]))
+                print(QPushButton(chat_list[x * 2 + 1]))
+        except Exception as e:
+            print(e)
+        """
+
+        a = QPushButton('hi')
+        vbox.addWidget(a)
+        a.clicked.connect(self.make_chat)
+
+        groupBox.setLayout(vbox)
+
+        return groupBox
+
+    def make_chat(self):
+        tmp = Chatting()
+        tmp.draw()
+
 
 class searchID(QWidget):
     def __init__(self):
@@ -413,6 +604,17 @@ class searchID(QWidget):
         self.show()
 
     def search_click(self):
+        friend_list = []
+
+        mysocket.sendMsg(s, '5', key)
+
+        while True:
+            string = mysocket.getMsg(s, key)
+            if string == 'end!' or string == 'end! ':
+                break
+            else:
+                friend_list.append(string)
+
         ID = self.input_search_ID.text()
 
         mysocket.sendMsg(s, '3', key)
@@ -437,6 +639,7 @@ class searchID(QWidget):
             QMessageBox.warning(self, '친구 찾기', '이미 추가되어 있는 사용자 입니다.')
             self.nickname.setText(self.input_search_ID.text()+' 님은\n이미 추가된 사용자 입니다.')
 
+
     def add_click(self):
         mysocket.sendMsg(s, '4', key)
         mysocket.sendMsg(s, self.input_search_ID.text(), key)
@@ -447,6 +650,7 @@ class searchID(QWidget):
 
         self.add_btn.setVisible(False)
         self.nickname.setText(self.input_search_ID.text() + ' 님은\n이미 추가된 사용자 입니다.')
+
 
 class withdrawID(QWidget):
 
@@ -481,20 +685,124 @@ class withdrawID(QWidget):
         if (answer == QMessageBox.No):
             return
         else:
-            QMessageBox.information(self, '와시발 ㅋㅋ', '엠창 탈퇴하네')
-            """
             mysocket.sendMsg(s, '2', key)
             mysocket.sendMsg(s, self.input_PW.text(), key)
             result = mysocket.getMsg(s, key)
             if result == 'PW different' or result == 'PW different ':
+                print(result)
                 QMessageBox.warning(self, '회원 탈퇴', '회원 탈퇴 실패')
                 return
             else:
                 QMessageBox.information(self, '회원 탈퇴', '지금까지 감사헀습니다. 안녕~')
-                로그인 폼으로.
-            """
+                self.hide()
+                tmp = MainWidget()
+                tmp.hide()
+                tmp = Login()
+                tmp.run()
 
 
+class makeRoom(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+    def makeroomUi(self):
+
+        friend_list = []
+
+        mysocket.sendMsg(s, '5', key)
+
+        while True:
+            string = mysocket.getMsg(s, key)
+            if string == 'end!' or string == 'end! ':
+                break
+            else:
+                friend_list.append(string)
+
+
+        self.friend_id = []
+
+        self.setGeometry(300, 100, 350, 550)
+        self.setWindowTitle('채팅방 만들기')
+
+        self.name_label = QLabel("채팅방 이름", self)
+        self.name_label.move(20, 20)
+
+        self.room_name = QLineEdit(self)
+        self.room_name.move(20, 40)
+        self.room_name.resize(310, 20)
+
+        self.name_label = QLabel("대화 상대", self)
+        self.name_label.move(20, 80)
+
+        self.room_member = QTextEdit(self)
+        self.room_member.setReadOnly(True)
+        self.room_member.move(20, 100)
+        self.room_member.resize(310, 50)
+
+        for x in range(int(len(friend_list)/2)):
+            tmp.append('')
+            tmp2.append('')
+
+        i = 20
+        j = 20
+
+        for x in range(len(tmp)):
+            tmp2[x] = friend_list[x*2]
+            tmp[x] = QCheckBox(friend_list[x*2+1], self)
+            tmp[x].move(i, 150+j)
+            tmp[x].toggle()
+            tmp[x].stateChanged.connect(self.print_list)
+            j += 20
+
+        self.make_btn = QPushButton('채팅 시작', self)
+        self.make_btn.resize(200, 20)
+        self.make_btn.move(20, 400)
+        self.make_btn.clicked.connect(self.make_click)
+
+        self.show()
+
+    def print_list(self, state):
+        string = ''
+        id_string = ''
+        nick_list = []
+
+        self.friend_id = []
+
+        if state == Qt.Checked:
+            for x in range(len(tmp)):
+                if tmp[x].isChecked() == True:
+                    string = tmp[x].text()
+                    nick_list.append(string)
+                    id_string = tmp2[x]
+                    self.friend_id.append(id_string)
+        else:
+            for x in range(len(tmp)):
+                if tmp[x].isChecked() == True:
+                    string = tmp[x].text()
+                    nick_list.append(string)
+                    id_string = tmp2[x]
+                    self.friend_id.append(id_string)
+
+        string = ''
+
+        for x in range(len(nick_list)):
+            temp = nick_list[x]
+            string = string + '\n' + temp
+        self.room_member.setPlainText(string)
+
+
+    def make_click(self):
+        if self.room_name.text() == '':
+            QMessageBox.warning(self, '채팅', '채팅방 이름을 입력해 주세요')
+            return
+        else:
+            mysocket.sendMsg(s, '7', key)
+            mysocket.sendMsg(s, self.room_name.text(), key)
+            for x in range(len(self.friend_id)):
+                mysocket.sendMsg(s, self.friend_id[x], key)
+            mysocket.sendMsg(s, 'end!', key)
+            self.hide()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
